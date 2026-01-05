f1a8c5ad5efd3f910fb358e4293051f48a155ba7
Alex <oleksi.ovsiannikov@gmail.com>
2026-01-05 09:46:30 +0100

    feat: complete sprint/fix

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,81 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+/* function calculateMedian(list) {
+
+  if (!Array.isArray(list))
+    return null;
+
+
+  const num = list.filter(
+    item => typeof item === 'number' && !Number.isNaN(item)
+  );
+
+  if (num.length === 0) { 
+
+    return null;
+  }
+
+
+  const temp = [...num];
+
+for (let i = 0; i <= middleIndex; i++) {
+  let minIndex = i;
+  for (let j = i+1; j < temp.length; j++) {
+    if (temp[j] < temp[minIndex]) minIndex = j;
+  }
+}
+ const tempVar = temp[i]
+ temp[i] = temp[minIndex]
+ temp[minIndex] = tempVar
+  
+
+
+ 
+
+
+const middleIndex = Math.floor(temp.length / 2);
+  if (temp.length % 2 !== 0) {
+    return temp[middleIndex];
+  } else {
+    return (temp[middleIndex - 1] + temp[middleIndex]) / 2;  // list[middleIndex - 1] // 40
+    }                                                //list[middleIndex]  50
+} */
+
+
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (!Array.isArray(list)) return null;
+
+  const num = list.filter(
+    item => typeof item === 'number' && !Number.isNaN(item)
+  );
+
+  if (num.length === 0) return null;
+
+  const temp = [...num];
+  const middleIndex = Math.floor(temp.length / 2);
+
+  for (let i = 0; i <= middleIndex; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < temp.length; j++) {
+      if (temp[j] < temp[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const tempVar = temp[i];
+    temp[i] = temp[minIndex];
+    temp[minIndex] = tempVar;
+  }
+
+  if (temp.length % 2 !== 0) {
+    return temp[middleIndex];
+  } else {
+    return (temp[middleIndex - 1] + temp[middleIndex]) / 2;
+  }
 }
 
 module.exports = calculateMedian;
+
+
+
---
+++ prep/prep_sprint_1/Arrays_workShop/exercises/exercise2.js
@@ -0,0 +1,8 @@
+// Write a function which takes an array as a parameter
+//   and swaps the first element with the last element
+
+function swapFirstAndLast(arr) {}
+
+const myArray = [5, 2, 3, 4, 1];
+swapFirstAndLast(myArray);
+console.log(myArray); // what output should we expect?
