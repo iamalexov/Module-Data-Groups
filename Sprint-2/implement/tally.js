d02c043b17d8f8220c611ea4e217f424204edac4
Alex <oleksi.ovsiannikov@gmail.com>
2026-01-18 21:27:59 +0100

    feat: complete implement/tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(a) {
+  let result = {};
+  for(var i = 0; i < a.length;i++) {
+    if(!result[a[i]]){
+
+         result[a[i]] = 0;
+    } 
+        result[a[i]]++;
+    
+  }
+  return result;
+
+}
 
 module.exports = tally;
+
+
+
+
